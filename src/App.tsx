a1178235fb207c78f3f631aef396143ab1fa1edb
Igor Rust <igorboiko4242@gmail.com>
2025-05-12 01:23:25 +0400

    circles++

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './styles.css'
+import math from './math';
 
 export default function App() {
-  // const [count, setCount] = useState(0)
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,29 +13,26 @@ export default function App() {
           Quaein
         </a>
         <a href="">
-          Quaein
-        </a>
-        <a href="">
-          Quaein
+          resume.pdf
         </a>
       </section>
 
-      <section className='flex flex-row'>
+      <section className='flex flex-col md:flex-row w-full max-w-[880px] gap-[120px]'>
 
-        <div>
-          <div className='h-[200px] w-[200px] bg-red-600 rounded-full' />
+        <div className='flex flex-col w-full justify-center items-center p-[42px]'>
+          <Circles />
         </div>
 
-        <div>
+        <div className='flex flex-col w-full gap-[28px]'>
 
           {/* Igor */}
-          <div className='flex flex-col'>
+          <div className='flex flex-col gap-[6px]'>
             <h1>Igor Boiko</h1>
             <h1 className='text-right'>Rust Dev</h1>
           </div>
 
           {/* About */}
-          <div className='flex flex-col'>
+          <div className='flex flex-col gap-[6px]'>
             <h2>About</h2>
             <div className='flex flex-row'>
               <div className='w-0 sm:w-[30%]'></div>
@@ -49,7 +44,7 @@ export default function App() {
           </div>
 
           {/* Contact */}
-          <div className='flex flex-col'>
+          <div className='flex flex-col gap-[6px]'>
             <h2>Get in touch</h2>
             <div className='flex flex-row self-end gap-x-4'>
               <a href="">
@@ -81,3 +76,49 @@ export default function App() {
   )
 }
 
+
+function Circles() {
+  const amount = 12;
+  const base_radius = 100;
+  const max_radius = 1000;
+
+  let circles = [];
+
+  for (let i = 0; i < amount; i++) {
+    const radius = Math.trunc(math.map_range(amount, 0, i, base_radius, max_radius));
+
+    if (i === amount - 1) {
+      circles.push(
+        <img
+          src={`./public/igor_avatar.jpg`}
+          key={i}
+          className={`${i % 2 === 0 ? "bg-black" : "bg-gray-500"} bg-cover rounded-full absolute`}
+          style={{
+            width: `${radius}px`,
+            height: `${radius}px`,
+            zIndex: i,
+          }}
+        />
+      );
+    } else {
+      circles.push(
+        <div
+          key={i}
+          className={`${i % 2 === 0 ? "bg-black" : "bg-gray-500"} rounded-full absolute`}
+          style={{
+            width: `${radius}px`,
+            height: `${radius}px`,
+            zIndex: i,
+          }}
+        />
+      );
+    }
+
+  }
+
+  return (
+    <div className='z-[-1] flex flex-col h-[330px] w-[330px] relative justify-center items-center'>
+      {circles}
+    </div>
+  );
+}
---
+++ src/math.ts
@@ -0,0 +1,21 @@
+export default class math {
+    static map_range =
+        (a1: number, a2: number, val: number, b1: number, b2: number): number =>
+            (val - a1) / (a2 - a1) * (b2 - b1) + b1;
+
+    static percentage =
+        (whole: number, percent: number): number =>
+            (whole / 100) * percent;
+
+    static lerp =
+        (a: number, b: number, t: number): number =>
+            a + t * (b - a);
+
+    static solveForC =
+        (a: number, b: number, d: number): number =>
+            (a * d) / b;
+
+    static solveForD =
+        (a: number, b: number, c: number): number =>
+            (b * c) / a;
+}
